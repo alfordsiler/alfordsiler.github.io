982ce3f42b01a8a11fd334c73049159fe796a19d
alfordsiler <alfordsiler@gmail.com>
2016-12-13 11:49:47 -0800

    removed unused resources cleaned up some js code small edits to portfolio descriptions cleaned up css code

--- js/script.js
+++ js/script.js
@@ -1,45 +1,5 @@
 $(document).ready(function(){
 
-  $(document).on('scroll',function(){
-    if (($(document).scrollTop()>=$('#about').offset().top-30 && $(document).scrollTop()<$('#portfolio').offset().top-30) ||
-       ($(document).scrollTop()>=$('#projects').offset().top-30 && $(document).scrollTop()<$('#contact').offset().top-30)){
-      $('.icon-white').removeClass('icon-white').addClass('icon-black');
-    }
-    else {
-      $('.icon-black').removeClass('icon-black').addClass('icon-white');
-    }
-  });
-
-  $('.icon').hover(function(){
-    $('.navTitle').show('slide', {direction: 'left'}, 50);
-  }, function () {
-    $('.navTitle').hide('slide', { direction: 'left'}, 50);
-  });
-
-  $('.icon1').hover(function(){
-    $('.navTitle1').show('slide', {direction: 'left'}, 50);
-  }, function () {
-    $('.navTitle1').hide('slide', { direction: 'left'}, 50);
-  });
-
-  $('.icon2').hover(function(){
-    $('.navTitle2').show('slide', {direction: 'left'}, 50);
-  }, function () {
-    $('.navTitle2').hide('slide', {direction: 'left'}, 50);
-  });
-
-  $('.icon3').hover(function(){
-    $('.navTitle3').show('slide', {direction: 'left'}, 50);
-  }, function () {
-    $('.navTitle3').hide('slide', {direction: 'left'}, 50);
-  });
-
-  $('.icon4').hover(function(){
-    $('.navTitle4').show('slide', {direction: 'left'}, 50);
-  }, function () {
-    $('.navTitle4').hide('slide', {direction: 'left'}, 50);
-  });
-
   $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -48,6 +8,68 @@ $(document).ready(function(){
         }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
+  });
+
+  $(document).on('scroll',function(){
+    if (($(document).scrollTop()>=$('#about').offset().top-30 && $(document).scrollTop()<$('#skills').offset().top-30) ||
+       ($(document).scrollTop()>=$('#skillsCubes').offset().top-30 && $(document).scrollTop()<$('#portfolio').offset().top-30) ||
+       ($(document).scrollTop()>=$('#projects').offset().top-30 && $(document).scrollTop()<$('#contact').offset().top-30)) {
+      $('.icon-white').removeClass('icon-white').addClass('icon-black');
+    }
+    else {
+      $('.icon-black').removeClass('icon-black').addClass('icon-white');
+    }
+  });
+
+  $('.icon').hover(function(){
+    $(this).siblings('.navTitle').show('slide', {direction: 'left'}, 50);
+  }, function () {
+    $(this).siblings('.navTitle').hide('slide', { direction: 'left'}, 50);
+  });
+
+  $(document).on('scroll',function(){
+    if ($(document).scrollTop()>=$('#home').offset().top-30 && $(document).scrollTop()<$('#about').offset().top-30) {
+      $('.glyphicon-home').addClass('pulse');
+    }
+    else {
+      $('.glyphicon-home').removeClass('pulse');
+    }
+  });
+
+  $(document).on('scroll',function(){
+    if ($(document).scrollTop()>=$('#about').offset().top-30 && $(document).scrollTop()<$('#skills').offset().top-30) {
+      $('.glyphicon-user').addClass('pulse');
+    }
+    else {
+      $('.glyphicon-user').removeClass('pulse');
+    }
+  });
+
+  $(document).on('scroll',function(){
+    if ($(document).scrollTop()>=$('#skills').offset().top-30 && $(document).scrollTop()<$('#portfolio').offset().top-30) {
+      $('.glyphicon-wrench').addClass('pulse');
+    }
+    else {
+      $('.glyphicon-wrench').removeClass('pulse');
+    }
+  });
+
+  $(document).on('scroll',function(){
+    if ($(document).scrollTop()>=$('#portfolio').offset().top-30 && $(document).scrollTop()<$('#contact').offset().top-30) {
+      $('.glyphicon-briefcase').addClass('pulse');
+    }
+    else {
+      $('.glyphicon-briefcase').removeClass('pulse');
+    }
+  });
+
+  $(document).on('scroll',function(){
+    if ($(document).scrollTop()>=$('#contact').offset().top-30) {
+      $('.glyphicon-envelope').addClass('pulse');
+    }
+    else {
+      $('.glyphicon-envelope').removeClass('pulse');
+    }
   });
 
   $('.darken').hover(function(){
